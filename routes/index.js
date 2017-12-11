9862f1e2d50b7ca73184c319cb9739b6530f66dd
Yeri <arifyeripratama@gmail.com>
2017-12-11 22:12:13 +0700

    add multer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
+var fs = require('fs')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+router.get('/profile', function (req, res, next) {
+  res.render('profile')
+})
+
+router.post('/profile', upload.single('avatar'), function (req, res, next) {
+  fs.rename(req.file.path, req.file.path + req.file.originalname)
+  res.send(req.file)
+})
+
+module.exports = router
